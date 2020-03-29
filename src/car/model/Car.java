0f642a1823fa86659cde87a7f001ef6048948650
Justinas <lisauskas.justinas@gmail.com>
2020-03-29 19:52:11 +0300

    fill model classes

--- src/car/model/Car.java
+++ src/car/model/Car.java
@@ -1,4 +1,99 @@
 package car.model;
 
 public class Car {
+    private int id;
+    private String make;
+    private String model;
+    private String feature;
+    private String fuel;
+    private int door;
+    private int userid;
+
+    public Car(int id, String make, String model, String feature, String fuel, int door, int userid) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.feature = feature;
+        this.fuel = fuel;
+        this.door = door;
+        this.userid = userid;
+    }
+
+    public Car(String make, String model, String feature, String fuel, int door, int userid) {
+        this.make = make;
+        this.model = model;
+        this.feature = feature;
+        this.fuel = fuel;
+        this.door = door;
+        this.userid = userid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getFeature() {
+        return feature;
+    }
+
+    public void setFeature(String feature) {
+        this.feature = feature;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getDoor() {
+        return door;
+    }
+
+    public void setDoor(int door) {
+        this.door = door;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    @Override
+    public String toString() {
+        return "Dakar{" +
+                "id=" + id +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", feature='" + feature + '\'' +
+                ", fueal='" + fuel + '\'' +
+                ", door=" + door +
+                ", userid=" + userid +
+                '}';
+    }
 }
--- src/car/model/CarDAO.java
+++ src/car/model/CarDAO.java
@@ -1,4 +1,92 @@
 package car.model;
 
+import java.sql.*;
+
 public class CarDAO {
+    final static String URL = "jdbc:mysql://localhost:3306/db";
+    public String add(Car car){
+        String query = "insert into car (make, model, feature, fuel, door, user_id)" +
+                "values (?,?,?,?,?,?)";
+        try {
+            Connection connection = DriverManager.getConnection(URL, "root", "");
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, car.getMake());
+            preparedStatement.setString(2, car.getModel());
+            preparedStatement.setString(3, car.getFeature());
+            preparedStatement.setString(4, car.getFuel());
+            preparedStatement.setInt(5, car.getDoor());
+            preparedStatement.setInt(6, car.getUserid());
+            preparedStatement.executeUpdate();
+
+            return "Successfully created new entry";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Failure creating new entry";
+        }
+    }
+
+    public ResultSet searchByMake(String make, User user){
+        String query2 = "";
+        if (user.isAdmin()) { // display all results- it's admin after all ;)
+            if (make.equals("")) {//Admin didn't entered any team_name - displaying all entries
+                query2 = "SELECT * FROM car";
+            } else {// Admin entered team_name displaying specific entries
+                query2 = "SELECT * FROM car WHERE make LIKE '" + make + "'";
+            }
+        } else { // display only specific user results
+            if (make.equals("")) {//User didn't entered any team_name - displaying all entries created by him
+                query2 = "SELECT * FROM car WHERE user_id = '" + user.getId() + "'";
+            } else {// User entered team_name displaying specific entries created by him
+                query2 = "SELECT * FROM car WHERE user_id = '" + user.getId() + "' AND make LIKE '" + make + "'";
+            }
+        }
+
+        ResultSet resultSet = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = DriverManager.getConnection(URL, "root", "");
+            preparedStatement = connection.prepareStatement(query2);
+            resultSet = preparedStatement.executeQuery(query2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultSet;
+    }
+
+    public void editById(Car car){
+        String query = "update car set make=?, model=?, feature=?, fuel=?, door=? " +
+                " where id=?";
+        try {
+            Connection connection = DriverManager.getConnection(URL, "root", "");
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, car.getMake());
+            preparedStatement.setString(2, car.getModel());
+            preparedStatement.setString(3, car.getFeature());
+            preparedStatement.setString(4, car.getFuel());
+            preparedStatement.setInt(5, car.getDoor());
+            preparedStatement.setInt(6, car.getId());
+            preparedStatement.executeUpdate();
+
+            System.out.println("Succeeded edit entry");
+        } catch (SQLException e) {
+            System.out.println("Failed edit entry");
+            e.printStackTrace();
+        }
+    }
+    public void deleteById(int id){
+        String query = "delete from car where id=?";
+        try {
+            Connection connection = DriverManager.getConnection(URL, "root", "");
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            System.out.println("Succes delete entry");
+        } catch (SQLException e) {
+            System.out.println("Failed delete entry");
+            e.printStackTrace();
+        }
+    }
 }
