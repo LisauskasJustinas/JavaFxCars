e9234287ad99dd39e00c06652848faf5b86bad5b
Justinas <lisauskas.justinas@gmail.com>
2020-03-29 20:00:23 +0300

    create and fill constant validation classes

--- src/car/utils/Constant.java
+++ src/car/utils/Constant.java
@@ -1,4 +1,7 @@
 package car.utils;
 
 public class Constant {
+    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String DB_NAME = "db";
+    public static final String TABLE_NAME = "user";
 }
